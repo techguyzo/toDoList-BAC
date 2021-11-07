77d747781de454ca536c5d5bdad69749851293d4
Rizzo <techguyyyyy@gmail.com>
2021-11-07 23:34:11 +0800

    to do list coding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      userName: 'Zo',
+      todoItems: [
+        {action: 'Buy Milk', done: false},
+        {action: 'Meeting', done: false},
+        {action: 'Jogging', done: false},
+      ],
+      newToDo: '',
+    };
+  }
+
+  updateValue = (event) => {
+    //set state. find newtodo, target input value
+    this.setState({newToDo: event.target.value});
+  };
+
+  newToDo = () => {
+    this.setState({
+      todoItems: [
+        ...this.state.todoItems,//call exsisting item
+        {action: this.state.newToDo, done: false},//new item to be merged and captured from state.newtodo
+      ],
+    });
+  };
+
+  toggleDone = (todo) => //data item pass into todo, at toggledone function. item (line 44) = todo
+  this.setState({
+    todoItems: this.state.todoItems.map((item) =>
+      item.action === todo.action //when item inside action is searched, it will be matched with todo action, inside todoitems
+      ? { ...item, done: !item.done } : item 
+      ),
+  });
+
+  todoRows = () => //take this data from state/todoitems. map will allow cycle through each item in array and extract them (for each item)
+    this.state.todoItems.map((item) => (
+      <tr key={item.action}>
+        <td>{item.action}</td>
+        <td>
+          <input type="checkbox" checked={item.done} onChange={() => this.toggleDone(item)/*pass item information to toggle done function*/} />
+        </td>
+      </tr>
+    ));
+
+  render = () => (
+    <div class="container">
+      <div className="row">
+        <div className="col-12">
+          <h2 className="bg-primary text-white text-center p2">
+            {this.state.userName}'s To do list
+          </h2>
+        </div>
+        <div className="col-12">
+          <input
+            className="form-control" value={this.state.newToDo} onChange={this.updateValue}
+          />
+          <button className="btn btn-primary" onClick={this.newToDo} /*on click, this will run newtodo function*/>
+            Add
+          </button>
+        </div>
+        <div className="col-12">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Task</th>
+                <th>Completed</th>
+              </tr>
+            </thead>
+            <tbody>{this.todoRows()}</tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
